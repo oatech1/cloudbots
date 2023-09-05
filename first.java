fad56ae41d32da9ce918d33eaf2b08689d2ab4cc
oatech <oayodele111@gmail.com>
2023-09-05 22:01:42 +0000

    vi commit

--- first.java
+++ first.java
@@ -1,4 +1,4 @@
 class first{
-	public static void main(String args[]){
-	System.out.println("Welcome to Java ");
-	}
+	public static void main(String[] args){
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to vi editor");
